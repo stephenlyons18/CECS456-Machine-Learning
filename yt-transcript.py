fbd611b975f09c3eb0ab2213631beb9860a83e18
stephenlyons18 <stephen.lyons18@gmail.com>
2022-11-08 22:49:45 -0800

    file name changes

--- yt-transcript.py
+++ yt-transcript.py
@@ -10,7 +10,7 @@
 # !apt -y install ffmpeg
 
 
-# In[ ]:
+# In[9]:
 
 
 from __future__ import unicode_literals
@@ -19,28 +19,36 @@ import whisper
 import warnings
 warnings.filterwarnings("ignore")
 
-# Download the video
-ydl_opts = {
-    'format': 'bestaudio/best',
-    'outtmpl': 'video.%(ext)s',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'wav',
-        'preferredquality': '192',
-    }],
-}
+
 link = input("Enter the link of the video: ")
 
 # get the title of the video
 with youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'}) as ydl:
     info_dict = ydl.extract_info(link, download=False)
     video_title = info_dict.get('title', None)
+    
+# clean the title of the video to use it as the name of the file (remove special characters)
+video_title = video_title.replace(" ", "_").replace(":", "").replace("?", "").replace("/", "").replace("\\", "").replace("|", "").replace("*", "").replace("<", "").replace(">", "").replace("\"", "").replace(".", "")
+
+
+# Download the video but only the audio part and optimize it for speech recognition and download speed into a .wav file using the title of the video
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'outtmpl': video_title + '.wav',
+    'keepvideo': False,
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'wav',
+        'preferredquality': '192',
+    }],
+}
 
 
 
-# In[ ]:
+# In[12]:
 
 
+# 
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     ydl.download([link])
 
@@ -54,16 +62,13 @@ model = whisper.load_model("base")
 
 
 # Transcribe the video into english text
-transcript = model.transcribe("./video.wav")
+transcript = model.transcribe("./" + video_title + ".wav")
 
 # Print the transcript
 print(transcript)
 
 
 # In[8]:
-# clean the title of the video to use it as the name of the file (remove special characters)
-video_title = video_title.replace(" ", "_").replace(":", "").replace("?", "").replace("/", "").replace("\\", "").replace("|", "").replace("*", "").replace("<", "").replace(">", "").replace("\"", "").replace(".", "")
-
 
 
 # save the transcript of the video in a text file with the name of the video
@@ -73,5 +78,5 @@ with open(video_title + ".txt", "w") as text_file:
 
 # delete the downloaded video file
 import os
-os.remove("video.wav")
+os.remove(video_title + ".wav")
 
